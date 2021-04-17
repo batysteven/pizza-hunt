da85cafa5791678c49df367fadc10953165feb5f
Steven Baty <baty_steven@yahoo.com>
2021-04-17 00:55:22 -0600

    add snapshot code, fixed routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const apiRoutes = require('./api');
 const htmlRoutes = require('./html/html-routes');
+const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
 router.use('/', htmlRoutes);
